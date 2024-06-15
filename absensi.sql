c364ab9e1171cf0c4a852aa6926b35544e68a4b1
br4masta <brianaldi2701@gmail.com>
2024-06-15 23:59:21 +0700

    db update

--- absensi.sql
+++ absensi.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jul 28, 2019 at 12:34 PM
--- Server version: 5.6.37
--- PHP Version: 5.6.25
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jun 2024 pada 18.58
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 7.3.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,153 +24,68 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `absensi`
+-- Struktur dari tabel `absensi`
 --
 
-CREATE TABLE IF NOT EXISTS `absensi` (
+CREATE TABLE `absensi` (
   `id_absen` int(11) NOT NULL,
   `tgl` date NOT NULL,
   `waktu` time NOT NULL,
   `keterangan` enum('Masuk','Pulang') NOT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `id_user` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `latitude` decimal(10,8) NOT NULL,
+  `longitude` decimal(11,8) NOT NULL,
+  `capture_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `jenis` int(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `absensi`
+-- Dumping data untuk tabel `absensi`
 --
 
-INSERT INTO `absensi` (`id_absen`, `tgl`, `waktu`, `keterangan`, `id_user`) VALUES
-(4, '2019-07-25', '07:21:53', 'Masuk', 6),
-(5, '2019-07-26', '09:00:47', 'Masuk', 6),
-(6, '2019-07-26', '16:01:03', 'Pulang', 6),
-(7, '2019-07-25', '17:01:28', 'Pulang', 6);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `divisi`
---
-
-CREATE TABLE IF NOT EXISTS `divisi` (
-  `id_divisi` smallint(3) NOT NULL,
-  `nama_divisi` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `divisi`
---
-
-INSERT INTO `divisi` (`id_divisi`, `nama_divisi`) VALUES
-(1, 'Marketing'),
-(2, 'IT '),
-(3, 'Akuntan'),
-(4, 'Kebersihan');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jam`
---
-
-CREATE TABLE IF NOT EXISTS `jam` (
-  `id_jam` tinyint(1) NOT NULL,
-  `start` time NOT NULL,
-  `finish` time NOT NULL,
-  `keterangan` enum('Masuk','Pulang') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `jam`
---
-
-INSERT INTO `jam` (`id_jam`, `start`, `finish`, `keterangan`) VALUES
-(1, '06:00:00', '08:15:00', 'Masuk'),
-(2, '17:00:00', '19:00:00', 'Pulang');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` smallint(5) NOT NULL,
-  `nik` varchar(20) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `telp` varchar(15) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `foto` varchar(20) DEFAULT 'no-foto.png',
-  `divisi` smallint(5) DEFAULT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `level` enum('Manager','Karyawan') NOT NULL DEFAULT 'Karyawan'
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id_user`, `nik`, `nama`, `telp`, `email`, `foto`, `divisi`, `username`, `password`, `level`) VALUES
-(1, '12312312332123', 'Ahmad Fadillah 1', '08139212092', 'ahm.fadil@mail.com', '1564316194.png', NULL, 'ahmad', '$2y$10$UwqloCX7PFLM3aQvgQxh6e9UgifqwQOZiF1zdogtLF6iVDR7Yr7IW', 'Manager'),
-(6, '123456789101112', 'Anissa Rahma', '08151231902', 'anissa.rhm31@mail.com', '1564293217.png', 1, 'anissa', '$2y$10$2kexYaZKVXX/5Liljm2FXO0Zk7BI5LUQgOTz1bRIf211eraxpju2a', 'Karyawan'),
-(7, '1231231231231123', 'Willy', '081231238', 'willy@mail.com', 'no-foto.png', 2, 'willy', '$2y$10$zGwtkQ8uCLBCmXEbPjJWF.vZkVlf7nmxhbe9iYu08wE8qkJNPdMb.', 'Karyawan'),
-(8, '8931289124891', 'Manager 1', '', '', 'no-foto.png', NULL, 'manager_1', '$2y$10$XtMY01KEOd5I065s8Exs0OcQ373RvRNG1JznORr6TmmBNWnZ3vjjK', 'Manager'),
-(9, '1231231238900', 'Manager 2', '', '', 'no-foto.png', NULL, 'manager_2', '$2y$10$iJWUOXDznGEmxo.bqnhtmeFL51jN5130LfDlKg8VROfoEmlgC.cFW', 'Manager'),
-(10, '908121310291', 'Manager 3', '', '', 'no-foto.png', NULL, 'manager_3', '$2y$10$uGsLvgl.6ji2iZ7tWkNvPelTwZdLQ6QA81Yawa20wsLairCXqV8BO', 'Manager'),
-(11, '123801204012', 'Manager 4', '', '', 'no-foto.png', NULL, 'master_4', '$2y$10$Kot81WNqrho4WlcYI13kT.Y5V2sMg1ZSAXcITrp8cj3dqHpbl4vrS', 'Manager');
+INSERT INTO `absensi` (`id_absen`, `tgl`, `waktu`, `keterangan`, `id_user`, `file_path`, `latitude`, `longitude`, `capture_time`, `jenis`) VALUES
+(4, '2019-07-25', '07:21:53', 'Masuk', 6, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(5, '2019-07-26', '09:00:47', 'Masuk', 6, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(6, '2019-07-26', '16:01:03', 'Pulang', 6, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(7, '2019-07-25', '17:01:28', 'Pulang', 6, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(8, '2021-02-15', '11:15:15', 'Masuk', 13, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(9, '2021-02-15', '11:15:52', 'Pulang', 13, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(10, '2021-02-15', '11:16:29', 'Pulang', 13, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(11, '2021-02-15', '05:17:06', 'Pulang', 13, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(12, '2021-02-15', '11:59:04', 'Masuk', 16, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(13, '2021-02-15', '11:59:09', 'Pulang', 16, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(14, '2021-02-15', '11:59:21', 'Masuk', 17, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(16, '2021-02-15', '11:59:38', 'Masuk', 18, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(17, '2021-02-15', '11:59:43', 'Pulang', 18, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(18, '2021-02-15', '11:59:56', 'Masuk', 19, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(19, '2021-02-15', '12:00:00', 'Pulang', 19, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(20, '2021-02-15', '12:04:36', 'Masuk', 20, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(21, '2021-02-15', '12:04:45', 'Pulang', 20, '', '0.00000000', '0.00000000', '2024-06-15 13:45:48', 0),
+(40, '2024-06-15', '23:53:26', 'Masuk', 16, 'uploads/snapshot_666dc70617de1.png', '-7.31054080', '112.72847360', '2024-06-15 16:53:26', 0),
+(41, '2024-06-15', '23:55:35', 'Pulang', 16, '', '-7.31054080', '112.72847360', '2024-06-15 16:55:35', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `absensi`
+-- Indeks untuk tabel `absensi`
 --
 ALTER TABLE `absensi`
   ADD PRIMARY KEY (`id_absen`);
 
 --
--- Indexes for table `divisi`
---
-ALTER TABLE `divisi`
-  ADD PRIMARY KEY (`id_divisi`);
-
---
--- Indexes for table `jam`
---
-ALTER TABLE `jam`
-  ADD PRIMARY KEY (`id_jam`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `absensi`
+-- AUTO_INCREMENT untuk tabel `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `divisi`
---
-ALTER TABLE `divisi`
-  MODIFY `id_divisi` smallint(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `jam`
---
-ALTER TABLE `jam`
-  MODIFY `id_jam` tinyint(1) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id_user` smallint(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
