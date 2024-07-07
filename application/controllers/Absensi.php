8718d225ae92b24aa5f20cfc335d9b416eb97b92
br4masta <brianaldi2701@gmail.com>
2024-07-07 19:11:51 +0700

    print excel

--- application/controllers/Absensi.php
+++ application/controllers/Absensi.php
@@ -209,7 +209,7 @@ class Absensi extends CI_Controller
         $style_row_libur = [
             'fill' => [
                 'type' => PHPExcel_Style_Fill::FILL_SOLID,
-                'color' => ['rgb' => '343A40']
+                'color' => ['rgb' => 'fff']
             ],
             'font' => [
                 'color' => ['rgb' => 'FFFFFF']
@@ -317,6 +317,113 @@ class Absensi extends CI_Controller
                 $excel->getActiveSheet()->getStyle('C'.$numrow)->applyFromArray($style_row);
                 $excel->getActiveSheet()->getStyle('D'.$numrow)->applyFromArray($style_row);
             }
+            $numrow++;
+        }
+
+        $excel->getActiveSheet()->getColumnDimension('A')->setWidth(5);
+        $excel->getActiveSheet()->getColumnDimension('B')->setWidth(25);
+        $excel->getActiveSheet()->getColumnDimension('C')->setWidth(25);
+        $excel->getActiveSheet()->getColumnDimension('D')->setWidth(25);
+        $excel->getActiveSheet()->getDefaultRowDimension()->setRowHeight(-1);
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment; filename="Absensi ' . $data['karyawan']->nama . ' - ' . bulan($data['bulan']) . ' ' . $data['tahun'] . '.xlsx"'); // Set nama file excel nya
+        header('Cache-Control: max-age=0');
+
+        $write = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+        $write->save('php://output');
+    }
+
+    public function export_excelNew()
+    {
+        include_once APPPATH . 'third_party/PHPExcel.php';
+        $data = $this->detail_data_absen();
+        $hari = $data['hari'];
+        $absen = $data['absen'];
+        $excel = new PHPExcel();
+
+        $excel->getProperties()
+                ->setCreator('IndoExpress')
+                ->setLastModifiedBy('IndoExpress')
+                ->setTitle('Data Absensi')
+                ->setSubject('Absensi')
+                ->setDescription('Absensi' . $data['karyawan']->nama . ' bulan ' . bulan($data['bulan']) . ', ' . $data['tahun'])
+                ->setKeyWords('data absen');
+
+        $style_col = [
+            'font' => ['bold' => true,'color' => ['rgb' => '000000']],
+            'alignment' => [
+                'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+            ],
+            'borders' => [
+                'top' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+                'bottom' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+                'right' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+                'left' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+            ]
+        ];
+
+        $style_row = [
+            'alignment' => [
+                'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+            ],
+            'borders' => [
+                'top' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+                'bottom' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+                'right' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+                'left' => ['style' => PHPExcel_Style_Border::BORDER_THIN],
+            ]
+        ];
+
+        
+
+        $excel->setActiveSheetIndex(0)->setCellValue('A1', 'Nama : ' . $data['karyawan']->nama);
+        $excel->getActiveSheet()->mergeCells('A1:D1');
+        $excel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+        $excel->getActiveSheet()->getStyle('A1')->getFont()->setSize(12);
+
+        $excel->setActiveSheetIndex(0)->setCellValue('A2', 'Divisi : ' . $data['karyawan']->nama_divisi);
+        $excel->getActiveSheet()->mergeCells('A2:D2');
+        $excel->getActiveSheet()->getStyle('A2')->getFont()->setBold(true);
+        $excel->getActiveSheet()->getStyle('A2')->getFont()->setSize(12);
+
+        $excel->setActiveSheetIndex(0)->setCellValue('A3', '');
+        $excel->getActiveSheet()->mergeCells('A3:D3');
+
+        $excel->setActiveSheetIndex(0)->setCellValue('A4', 'Data Absensi Bulan ' . bulan($data['bulan']) . ', ' . $data['tahun']);
+        $excel->getActiveSheet()->mergeCells('A4:D4');
+        $excel->getActiveSheet()->getStyle('A4')->getFont()->setBold(true);
+        $excel->getActiveSheet()->getStyle('A4')->getFont()->setSize(12);
+
+        $excel->setActiveSheetIndex(0)->setCellValue('A5', 'NO');
+        $excel->setActiveSheetIndex(0)->setCellValue('B5', 'Tanggal');
+        $excel->setActiveSheetIndex(0)->setCellValue('C5', 'Jam Masuk');
+        $excel->setActiveSheetIndex(0)->setCellValue('D5', 'Jam Keluar');
+
+        $excel->getActiveSheet()->getStyle('A5')->applyFromArray($style_col);
+        $excel->getActiveSheet()->getStyle('B5')->applyFromArray($style_col);
+        $excel->getActiveSheet()->getStyle('C5')->applyFromArray($style_col);
+        $excel->getActiveSheet()->getStyle('D5')->applyFromArray($style_col);
+
+        $numrow = 6;
+        foreach ($hari as $i => $h) {
+            $absen_harian = array_search($h['tgl'], array_column($absen, 'tgl')) !== false ? $absen[array_search($h['tgl'], array_column($absen, 'tgl'))] : '';
+
+            $excel->setActiveSheetIndex(0)->setCellValue('A'.$numrow, ($i+1));
+            $excel->setActiveSheetIndex(0)->setCellValue('B'.$numrow, $h['hari'] . ', ' . $h['tgl']);
+
+            $jamMasuk = check_jamNew(@$absen_harian['jam_masuk'], 'Masuk',true,@$absen_harian['id_absen'],@$absen_harian['latitude_masuk'],@$absen_harian['longitude_masuk'],@$absen_harian['user_name'],$absen_harian);
+            $jamPulang = check_jamNew(@$absen_harian['jam_pulang'], 'Pulang',true,@$absen_harian['id_absen_pulang'],@$absen_harian['latitude_pulang'],@$absen_harian['longitude_pulang'],@$absen_harian['user_name'],$absen_harian);
+
+            $excel->setActiveSheetIndex(0)->setCellValue('C'.$numrow, $jamMasuk['text']);
+            $excel->setActiveSheetIndex(0)->setCellValue('D'.$numrow, $jamPulang['text']);
+
+            
+            $excel->getActiveSheet()->getStyle('A'.$numrow)->applyFromArray($style_row);
+            $excel->getActiveSheet()->getStyle('B'.$numrow)->applyFromArray($style_row);
+            $excel->getActiveSheet()->getStyle('C'.$numrow)->applyFromArray($style_row);
+            $excel->getActiveSheet()->getStyle('D'.$numrow)->applyFromArray($style_row);
             $numrow++;
         }
 
